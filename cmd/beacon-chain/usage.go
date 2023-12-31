d7a8d8b8e046d7765e30792a73e6cf86121d42a4
sager <xueqian1991@gmail.com>
2023-12-31 17:55:21 +0800

    add flag

--- cmd/beacon-chain/usage.go
+++ cmd/beacon-chain/usage.go
@@ -153,6 +153,7 @@ var appHelpFlagGroups = []flagGroup{
 			cmd.P2PHostDNS,
 			cmd.P2PMaxPeers,
 			cmd.P2PPrivKey,
+			cmd.P2PPrivHex,
 			cmd.P2PStaticID,
 			cmd.P2PMetadata,
 			cmd.P2PAllowList,
